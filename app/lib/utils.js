71494c42c265ffc921e64d4a6df2c7afeef13fdb
Joan Lasierra Miró <joan@lasierra.cat>
2021-06-16 12:37:18 +0200

    #40605 Wefire Mobile: Images are not visible

--- app/lib/utils.js
+++ app/lib/utils.js
@@ -3,13 +3,17 @@ import {
 } from 'semver';
 
 export const formatAttachmentUrl = (attachmentUrl, userId, token, server) => {
+	attachmentUrl = decodeURI(attachmentUrl);
 	if (attachmentUrl.startsWith('http')) {
 		if (attachmentUrl.includes('rc_token')) {
-			return encodeURI(attachmentUrl);
+			// return encodeURI(attachmentUrl);
+			return attachmentUrl;
 		}
-		return encodeURI(`${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`);
+		// return encodeURI(`${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`);
+		return attachmentUrl + encodeURI(`?rc_uid=${ userId }&rc_token=${ token }`);
 	}
-	return encodeURI(`${ server }${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`);
+	// return encodeURI(`${ server }${ attachmentUrl }?rc_uid=${ userId }&rc_token=${ token }`);
+	return `${ server }${ attachmentUrl }${ encodeURI(`?rc_uid=${ userId }&rc_token=${ token }`) }`;
 };
 
 export const methods = {
